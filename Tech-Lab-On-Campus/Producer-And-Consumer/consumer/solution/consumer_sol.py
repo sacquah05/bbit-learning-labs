744384d39c8411eec94d34b5fa0f0cde20bdfd64
msolo-st <msolo5@illinois.edu>
2024-03-07 19:56:38 -0600

    add consumer

--- Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
+++ Tech-Lab-On-Campus/Producer-And-Consumer/consumer/solution/consumer_sol.py
@@ -1,29 +1,19 @@
-# Copyright 2024 Bloomberg Finance L.P.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from consumer_interface import mqConsumerInterface
 import pika
 import os
 
-class mqConsumerInterface:
+class mqConsumer(mqConsumerInterface):
     def __init__(
         self, binding_key: str, exchange_name: str, queue_name: str
     ) -> None:
         # Save parameters to class variables
-        
-        # Call setupRMQConnection
-        setupRMQConnection()
-        pass
+        self.binding_key = binding_key
+        self.queue_name = queue_name
+        self.exchange_name = exchange_name
 
+        self.setupRMQConnection()
+        pass
+    
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
         con_params = pika.URLParameters(os.environ["AMQP_URL"])
@@ -42,7 +32,7 @@ class mqConsumerInterface:
             )
         # Set-up Callback function for receiving messages
         self.channel.basic_consume(
-            "Queue Name", setupRMQConnection, auto_ack=False
+            "Queue Name", self.on_message_callback, auto_ack=False
         )
         pass
 
@@ -61,13 +51,5 @@ class mqConsumerInterface:
         # Start consuming messages
         self.channel.start_consuming()
         pass
+
     
-    ''' def __del__(self) -> None:
-        # Print "Closing RMQ connection on destruction"
-        print("Closing RMQ connection on destruction")
-        # Close Channel
-        self.channel.close()
-        # Close Connection
-        self.connection.close()
-        pass
-        '''
